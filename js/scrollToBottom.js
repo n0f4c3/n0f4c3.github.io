9e71348a9f90706cbeeefdb78b0aef7d9be23a1f
David <dptcool@gmail.com>
2023-04-19 11:39:55 -0400

    Correction pour la sécurité anticopie du site

--- js/scrollToBottom.js
+++ js/scrollToBottom.js
@@ -1,24 +1,24 @@
-function scrollToBottom() {
-			window.scrollTo({
-				top: document.documentElement.scrollHeight,
-				behavior: 'smooth'
-			});
-		}
+window.addEventListener('load', function() {
+  // empêcher la sélection de texte
+  if (typeof document.onselectstart != "undefined") {
+    document.onselectstart = function() { return false; };
+  } else if (typeof document.style.MozUserSelect != "undefined") {
+    document.style.MozUserSelect = "none";
+  } else {
+    document.onmousedown = function() { return false; };
+  }
+});
 
 document.addEventListener('contextmenu', function(event) {
   event.preventDefault(); //empêche l'affichage du menu contextuel
   alert('Merci de respecter le travail du webmaster en ne copiant pas le contenu sans autorisation');
 });
 
-function disableSelection() {
-    // empêcher la sélection de texte
-    if (typeof document.onselectstart != "undefined") {
-        document.onselectstart = function() { return false; };
-    } else if (typeof document.style.MozUserSelect != "undefined") {
-        document.style.MozUserSelect = "none";
-    } else {
-        document.onmousedown = function() { return false; };
-    }
-}
+function scrollToBottom() {
+			window.scrollTo({
+				top: document.documentElement.scrollHeight,
+				behavior: 'smooth'
+			});
+		}
 
 		
