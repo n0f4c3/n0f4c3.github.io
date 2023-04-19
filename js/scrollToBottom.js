f88e696a2bab1c772a43d0ac45f8429d9a0d1938
David <dptcool@gmail.com>
2023-04-19 09:42:09 -0400

    Ajout d'une sécurité anticopie

--- js/scrollToBottom.js
+++ js/scrollToBottom.js
@@ -9,4 +9,16 @@ document.addEventListener('contextmenu', function(event) {
   event.preventDefault(); //empêche l'affichage du menu contextuel
   alert('Merci de respecter le travail du webmaster en ne copiant pas le contenu sans autorisation');
 });
+
+function disableSelection() {
+    // empêcher la sélection de texte
+    if (typeof document.onselectstart != "undefined") {
+        document.onselectstart = function() { return false; };
+    } else if (typeof document.style.MozUserSelect != "undefined") {
+        document.style.MozUserSelect = "none";
+    } else {
+        document.onmousedown = function() { return false; };
+    }
+}
+
 		
