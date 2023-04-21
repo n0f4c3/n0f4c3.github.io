fa58484604d40cdef37e0ea18622c47e8374c446
David <dptcool@gmail.com>
2023-04-21 16:17:35 -0400

    Mise à jour pour le site 21/04/2023

--- js/modale.js
+++ js/modale.js
@@ -1,11 +1,31 @@
-  var dialog = document.getElementById('modale');
-  var openBtn = document.getElementById('openBtn');
-  var closeBtn = document.getElementById('closeBtn');
+// Récupérer le bouton pour ouvrir la fenêtre modale
+var openModalBtn = document.getElementById("openModalBtn");
 
-  openBtn.addEventListener('click', function() {
-    dialog.showModal();
-  });
+// Récupérer la fenêtre modale
+var modal = document.getElementById("modal");
 
-  closeBtn.addEventListener('click', function() {
-    dialog.close();
-  });
+// Récupérer le bouton pour fermer la fenêtre modale
+var closeBtn = document.getElementsByClassName("close")[0];
+
+// Fonction pour ouvrir la fenêtre modale
+function openModal() {
+  modal.style.display = "block";
+}
+
+// Fonction pour fermer la fenêtre modale
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// Ajouter un événement de clic sur le bouton pour ouvrir la fenêtre modale
+openModalBtn.addEventListener("click", openModal);
+
+// Ajouter un événement de clic sur le bouton pour fermer la fenêtre modale
+closeBtn.addEventListener("click", closeModal);
+
+// Ajouter un événement de clic en dehors de la fenêtre modale pour fermer la fenêtre
+window.addEventListener("click", function(event) {
+  if (event.target == modal) {
+    closeModal();
+  }
+});
